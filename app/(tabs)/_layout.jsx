06f60cbae27cf83550351fe560f6b593ae69a328
Prince Nmezi <stephenprince427@gmail.com>
2024-07-06 15:40:50 +0100

    added Tab navigation

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,9 +1,106 @@
-import React from 'react'
+import React from "react";
+import { Image, Text, View } from "react-native";
+import { icons } from "../../constants";
+import { Tabs } from "expo-router";
 
-const TabsLayout= () => {
+const TabIcon = ({ focused, color, name, icon }) => {
   return (
-    <Text>Layout</Text>
-  )
-}
+    <View className="items-center justify-center gap-2">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-7 h-7"
+      />
+      <View>
+        <Text
+          className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+          style={{ color }}
+        >
+          {name}
+        </Text>
+      </View>
+    </View>
+  );
+};
 
-export default TabsLayout
+const TabsLayout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: "#de890a",
+        tabBarInactiveTintColor: "#CDCDE0",
+        tabBarStyle: {
+          backgroundColor: "#161622",
+          borderTopWidth: 1,
+          borderTopColor: "#232533",
+          height: 84,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused, color }) => (
+            <TabIcon
+              focused={focused}
+              color={color}
+              name="Home"
+              icon={icons.home}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused, color }) => (
+            <TabIcon
+              focused={focused}
+              color={color}
+              name="Profile"
+              icon={icons.profile}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: "Create",
+          headerShown: false,
+          tabBarIcon: ({ focused, color }) => (
+            <TabIcon
+              focused={focused}
+              color={color}
+              name="Create"
+              icon={icons.upload}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="bookmark"
+        options={{
+          title: "Bookmarks",
+          headerShown: false,
+          tabBarIcon: ({ focused, color }) => (
+            <TabIcon
+              focused={focused}
+              color={color}
+              name="Bookmarks"
+              icon={icons.bookmark}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabsLayout;
--- app/index.jsx
+++ app/index.jsx
@@ -7,7 +7,7 @@ export default function App() {
     <View className="flex-1 justify-center items-center">
       <Text className="text-3xl font-pblack">CyberAi</Text>
       <StatusBar style="auto" />
-      <Link href="/profile">Go to profile</Link>
+      <Link href="/home">Go to Home</Link>
     </View>
   );
 }
